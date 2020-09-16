f022c2f6046263bb3c44e61c82800dde97591079
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-16 23:00:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Batman", "Superman", "Wonder Woman", "Cyborg"]
+  >>
 end
 
 def add_element_to_start_of_array(array, element)
