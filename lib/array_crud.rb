afb1115f6e74926123c72ffdc6eb2953db76e105
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-16 23:43:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  phrase = ["I", "am", "really", "learning", "arrays!"]
+  phrase = ["wow", "i", "am", "learning", "arrays!"]
   phrase[2]
 end
 
