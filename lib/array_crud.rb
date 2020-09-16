a270ce975dc070b4b6a1c5088f9f1c19d334cb18
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-16 23:02:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   jla = ["Batman", "Superman", "Wonder Woman", "Cyborg"]
-  << "Green Lantern"
+  jla << "Green Lantern"
 end
 
 def add_element_to_start_of_array(array, element)
