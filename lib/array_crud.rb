09608ff4b5e6a395b31acee186c179f0e8459248
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-16 23:32:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   phrase = ["Wow", "I'm", "really", "learning"]
-  phrase.unshift ("arrays!")
+  phrase.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
