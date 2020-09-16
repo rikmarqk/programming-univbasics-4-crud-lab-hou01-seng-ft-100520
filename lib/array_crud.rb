c3cbb5e473a60e1d9accad463cdfe6feeaf865d1
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-16 23:56:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,7 @@ end
 
 def retrieve_last_element_from_array(array)
     phrase = ["wow", "i", "am", "learning", "arrays!"]
-  phrase[0]
+  phrase[4]
 end
 
 def update_element_from_index(array, index_number, element)
